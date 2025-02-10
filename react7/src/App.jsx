4652a39cfa18476bd789333a6245dbef42e74c2d
saarthack <ssarthak2003@outlook.com>
2025-02-10 19:28:09 +0530

    task

---
+++ react7/src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [imageURL, setImageURL] = useState('')
+
+  const [allUsers, setAllUsers] = useState([])
+
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    const copyUser = [...allUsers]
+
+    copyUser.push({username,email,imageURL})
+
+    setAllUsers(copyUser)
+    
+
+    setUsername('')
+    setEmail('')
+    setImageURL('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+        <input
+          className='border-2 px-3 rounded py-1 m-2 inline-block'
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value)
+          }}
+          type="text"
+          placeholder='Enter name' />
+
+        <input
+          className='border-2 px-3 rounded py-1 m-2 inline-block'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+          type="text"
+          placeholder='Enter Email' />
+
+        <input
+          className=' border-2 px-3 rounded py-1 m-2 inline-block'
+          value={imageURL}
+          onChange={(e) => {
+            setImageURL(e.target.value)
+          }}
+          type="text"
+          placeholder='Paste Image URL' />
+
+        <button
+          className='bg-emerald-600 text-white border-2 px-3 rounded py-1 m-2 inline-block'
+        >Submit</button>
+      </form>
+
+      <div className='p-3'>
+        {allUsers.map(function (elem, idx) {
+
+          return <div key={idx}>
+            <h2>{elem.username}</h2>
+            <h4>{elem.email}</h4>
+            <img src={elem.imageURL} className='h-10' alt="" />
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
